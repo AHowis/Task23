3ac6cf3f5d8318c31747bf5563c60525809bd862
A Howis <157127523+AHowis@users.noreply.github.com>
2024-03-13 11:58:14 +0000

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+#ask user their name
+hey_there = input("Whats your name?: ")
+#print a greeting followed by the input
+print(f"Hey {hey_there} !")
+print(f" Woah! {hey_there} is such a cool name ! So jealous")
